e4a016432c287803828c714062b68c5ea8e2f42b
Dharani <dharani1414@gmail.com>
2024-05-27 10:04:19 -0700

    profile dropdown open/close functionality

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,25 +1,51 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const dropdownRef = useRef(null);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+            setShowMenu(false);
+        }
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
+
+
   return (
-    <>
-      <button>
+    <div>
+      <button onClick={toggleMenu}>
         <FaUserCircle />
       </button>
-      <ul className="profile-dropdown">
-          <button onClick={logout}>Log Out</button>
-      </ul>
-    </>
+      {showMenu && (
+        <ul className="profile-dropdown" ref={dropdownRef}>
+          <li>
+            <button onClick={logout}>Log Out</button>
+          </li>
+        </ul>
+      )}
+    </div>
   );
 }
 
