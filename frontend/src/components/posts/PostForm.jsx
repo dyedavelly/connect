5ec88da58931be3620d0f059df23459fc3d09d1e
Dharani <dharani1414@gmail.com>
2024-06-03 15:55:41 -0700

    creating post

--- frontend/src/components/posts/PostForm.jsx
+++ frontend/src/components/posts/PostForm.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './PostForm.scss';
 import PostsIndex from './PostsIndex';
+import * as postActions from '../../store/posts';
 
 function PostForm() {
+    const dispatch = useDispatch();
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [body, setBody] = useState("");
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -12,6 +16,11 @@ function PostForm() {
     const closeModal = () => {
         setIsModalOpen(false);
     };
+
+    const createPost = () => {
+       dispatch(postActions.createPost({ body }));
+       closeModal();
+    }
 
     return (
         <>
@@ -25,8 +34,8 @@ function PostForm() {
                 <div className="modal-overlay">
                     <div className="modal">
                         <button className="close-button" onClick={closeModal}>X</button>
-                        <textarea placeholder="what do you want to talk about?"></textarea>
-                        <button className="post-submit-button">Post</button>
+                        <textarea placeholder="what do you want to talk about?" onChange={(e) => setBody(e.target.value)}></textarea>
+                        <button onClick={createPost} className="post-submit-button">Post</button>
                     </div>
                 </div>
             )}
