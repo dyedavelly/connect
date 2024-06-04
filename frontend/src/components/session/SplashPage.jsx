6012a575ad40eb8561346d50dea6f21f5040df19
Dharani <dharani1414@gmail.com>
2024-06-04 16:28:00 -0700

    added image on home page

--- frontend/src/components/session/SplashPage.jsx
+++ frontend/src/components/session/SplashPage.jsx
@@ -40,22 +40,28 @@ function SplashPage() {
   return (
     <>
     <div className="main-container">
-    <p className="home-page-heading">Welcome to your professional community</p>
-      <div className="sign-in-container">
-        <form onSubmit={handleSubmit}>
-          <ul>
-            {errors.map(error => <li key={error}>{error}</li>)}
-          </ul>
-          <label>Email</label>
-          <input type="text" className="input-field" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-          <label>Password</label>
-          <input type="password" className="input-field" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-          <button type="submit" className="sign-in-button">Sign in</button>
-        </form>
-        <p className="join-now"><Link to={"/signup"}>New to connect? Join now</Link></p>
-        <button onClick={handleClick} className="demo-login-button">Login as Demo User</button>
+      <div className='home-container'>
+        <p className="home-page-heading">Welcome to your professional community</p>
+        <div className="sign-in-container">
+          <form onSubmit={handleSubmit}>
+            <ul>
+              {errors.map(error => <li key={error}>{error}</li>)}
+            </ul>
+            <label>Email</label>
+            <input type="text" className="input-field" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+            <label>Password</label>
+            <input type="password" className="input-field" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+            <button type="submit" className="sign-in-button">Sign in</button>
+          </form>
+          <p className="join-now"><Link to={"/signup"}>New to connect? Join now</Link></p>
+          <button onClick={handleClick} className="demo-login-button">Login as Demo User</button>
+        </div>
+      </div> 
+      <div>
+       <img className="home-page-image" src="/home-page-image.png" alt="image" />
       </div>
-    </div> 
+      </div>
+    
     
     </>
   );
